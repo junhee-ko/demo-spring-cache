5306974e860a2bb70f694c73fa8914b16d825e26
junhee-ko <junheee.ko@gmail.com>
2023-09-27 19:19:49 +0900

    first commit

---
+++ src/main/kotlin/com/example/demospringcache/DemoSpringCacheApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demospringcache
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoSpringCacheApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoSpringCacheApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demospringcache/DemoSpringCacheApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demospringcache
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoSpringCacheApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
